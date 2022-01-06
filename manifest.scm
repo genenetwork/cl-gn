39c949a97ea2f474f6242b9f343fc78fa97c64bf
jgart <jgart@dismail.de>
2022-01-06 10:30:15 +0530

    manifest.scm: Use specifications->manifest.
    * manifest.scm: Use specifications->manifest instead of
    packages->manifest.
    
    Signed-off-by: Arun Isaac <arunisaac@systemreboot.net>

--- manifest.scm
+++ manifest.scm
@@ -1,5 +1,6 @@
-(use-modules (gnu packages lisp)
-             (gnu packages lisp-xyz))
-
-(packages->manifest
- (list sbcl sbcl-cl-json sbcl-hunchentoot sbcl-legit sbcl-slynk))
+(specifications->manifest
+ (list "sbcl"
+       "sbcl-cl-json"
+       "sbcl-hunchentoot"
+       "sbcl-legit"
+       "sbcl-slynk"))
